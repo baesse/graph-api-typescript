8481b3f20a446df164311e1e0ef476e0ae43ed10
root <root@DESKTOP-VMPOCD9.localdomain>
2019-08-31 12:35:22 -0300

    Initial express config

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
-console.log("hello world");
-let a = "hesddsdsdsdssdsdsdssasadsdsaslllo1111111111dsdsds1";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    middleware() {
+        this.express.use("/hello", (req, res, next) => {
+            res.send({
+                hello: "Hello world!"
+            });
+        });
+    }
+}
+exports.default = new App().express;
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const app_1 = require("./app");
+const server = http.createServer(app_1.default);
+server.listen(3000);
+server.on("listening", () => {
+    console.log("Running on port 3000....");
+});
